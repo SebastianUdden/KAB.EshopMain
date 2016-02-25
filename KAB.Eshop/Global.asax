d6e811df18c3ee32672734e835a12ce70b9663c3
SebastianUdden <sebastian.udden@gmail.com>
2016-02-25 09:22:30 +0100

    Made a change

---
+++ KAB.Eshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KAB.Eshop.Global" Language="C#" %>
---
+++ KAB.Eshop/app.js
@@ -0,0 +1,42 @@
+﻿var eShopApp = angular.module('eShopApp', ['ngRoute', 'ngResource']);
+
+eShopApp.config(function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+
+    .when('/admin', {
+        templateUrl: 'pages/admin.html',
+        controller: 'adminController'
+    })
+
+    .when('/search-results/', {
+        templateUrl: 'pages/searchResults.html',
+        controller: 'searchQueryController'
+    })
+});
+
+eShopApp.service('cityService', function() {
+    this.searchResult = "";
+});
+
+eShopApp.controller('headerController', ['$scope', 'cityService', function ($scope, cityService) {
+    $scope.searchResult = cityService.searchResult;
+
+    $scope.$watch('searchResult', function () {
+        cityService.searchResult = $scope.searchResult;
+    });
+}]);
+
+
+eShopApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+}]);
+
+//eShopApp.controller('adminController', ['$scope', function($scope) {
+//}]);
+
+eShopApp.controller('searchQueryController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+    $scope.searchResult = cityService.searchResult;
+}]);
