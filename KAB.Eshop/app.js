36324502e482fba5ea12d68a19505752ada77458
SebastianUdden <sebastian.udden@gmail.com>
2016-02-26 11:14:19 +0100

    Added tons of stuff

--- KAB.Eshop/app.js
+++ KAB.Eshop/app.js
@@ -26,35 +26,46 @@ eShopApp.config(function($routeProvider) {
         templateUrl: 'pages/shoppingCart.html',
         controller: 'shoppingCartController'
     })
+
+    .when('/user-log-in', {
+        templateUrl: 'pages/userLogin.html',
+        controller: 'userLoginController'
+    })
+
+    .when('/user-sign-up', {
+        templateUrl: 'pages/userRegister.html',
+        controller: 'userRegisterController'
+    })
 });
 
-eShopApp.service('cityService', function() {
+eShopApp.service('shopService', function() {
     this.searchResult = "";
 });
 
-eShopApp.controller('headerController', ['$scope', 'cityService', function ($scope, cityService) {
-    $scope.searchResult = cityService.searchResult;
+eShopApp.controller('headerController', ['$scope', 'shopService', function ($scope, shopService) {
+    $scope.searchResult = shopService.searchResult;
 
     $scope.$watch('searchResult', function () {
-        cityService.searchResult = $scope.searchResult;
+        shopService.searchResult = $scope.searchResult;
     });
 }]);
 
 
-eShopApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+eShopApp.controller('homeController', ['$scope', 'shopService', function($scope, shopService) {
 }]);
 
 //eShopApp.controller('adminController', ['$scope', function($scope) {
 //}]);
 
-eShopApp.controller('searchQueryController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
-    $scope.searchResult = cityService.searchResult;
+eShopApp.controller('searchQueryController', ['$scope', '$resource', 'shopService', function($scope, $resource, shopService) {
+    $scope.searchResult = shopService.searchResult;
 }]);
 
-eShopApp.controller('devLinksController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService) {
-    $scope.searchResult = cityService.searchResult;
+eShopApp.controller('devLinksController', ['$scope', '$resource', 'shopService', function ($scope, $resource, shopService) {
 }]);
 
-eShopApp.controller('shoppingCartController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService) {
-    $scope.searchResult = cityService.searchResult;
+eShopApp.controller('shoppingCartController', ['$scope', '$resource', 'shopService', function ($scope, $resource, shopService) {
+}]);
+
+eShopApp.controller('userRegisterController', ['$scope', '$resource', 'shopService', function ($scope, $resource, shopService) {
 }]);
