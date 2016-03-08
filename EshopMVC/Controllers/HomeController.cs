2dd4ebf83160a965b9a161ac4e487e77677db4c2
VIvst <amin.v.naeri@gmail.com>
2016-03-08 16:04:11 +0100

    Context, HttpGet-Create, HttpPost-Create, Indext-return View

--- EshopMVC/Controllers/HomeController.cs
+++ EshopMVC/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using EshopMVC.Models;
+using EshopMVC.ViewModel;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,7 +12,28 @@ namespace EshopMVC.Controllers
 {
     public class HomeController : Controller
     {
+        CustomerContext context;
+        public HomeController(CustomerContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
         // GET: /<controller>/
+        [HttpPost]
+        public IActionResult Create(CreateCustomerViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+                var dataManager = new DataManager(context);
+                dataManager.AddCustomer(viewModel);
+                return RedirectToAction(nameof(HomeController.Index));
+        }
         public IActionResult Index()
         {
             return View();
