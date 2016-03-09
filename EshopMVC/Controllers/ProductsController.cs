1dc825e3b34d903d4494d9bb7ef228f2a5090f92
Jacob Strömbäck <jacob.stromback@gmail.com>
2016-03-09 11:48:37 +0100

    productscontroller

---
+++ EshopMVC/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using EshopMVC.Models;
+using EshopMVC.ViewModel;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EshopMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductContext context;
+        public ProductsController(ProductContext context)
+        {
+            this.context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(CreateProductViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var productDataManager = new ProductDataManager(context);
+            productDataManager.AddProduct(viewModel);
+
+            return RedirectToAction(nameof(HomeController.Index));
+        }
+    }
+}
