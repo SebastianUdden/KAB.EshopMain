fac806925e79f549f451fbc8ab94b79aaf86a697
Jacob Strömbäck <jacob.stromback@gmail.com>
2016-03-09 11:25:15 +0100

    Product datamanager

---
+++ EshopMVC/Models/ProductDataManager.cs
@@ -0,0 +1,63 @@
+﻿using EshopMVC.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EshopMVC.Models
+{
+    public class ProductDataManager
+    {
+        ProductContext Context;
+
+        public ProductDataManager(ProductContext context)
+        {
+            this.Context = context;
+        }
+        public void AddProduct(CreateProductViewModel viewModel)
+        {
+            Product p = new Product();
+            p.ProductName = viewModel.ProductName;
+            p.Price = viewModel.Price;
+            p.PictureLink = viewModel.ImageURL;
+            p.ProductDescription = viewModel.ProductDescription;
+            p.Stock = viewModel.AmountToAdd;
+            p.CategoryId = viewModel.Category;
+
+            Context.Products.Add(p);
+
+        }
+
+        public Product GetProductById(int id)
+        {
+            //this needs testing, *should* return a product object from the database
+            var Product = Context.Products.Select(x => new Product
+            {
+                Id = x.Id,
+                ProductName = x.ProductName,
+                ProductDescription = x.ProductDescription,
+                Price = x.Price,
+                PictureLink = x.PictureLink,
+                Stock = x.Stock,
+                CategoryId = x.CategoryId,
+            }).ToArray();
+            return Product[0];
+        }
+
+        public Product[] GetProductByCategory(int categoryId)
+        {
+            //untested
+            var Product = Context.Products.Select(x => new Product
+            {
+                Id = x.Id,
+                ProductName = x.ProductName,
+                ProductDescription = x.ProductDescription,
+                Price = x.Price,
+                PictureLink = x.PictureLink,
+                Stock = x.Stock,
+                CategoryId = x.CategoryId,
+            }).ToArray();
+            return Product;
+        }
+    }
+}
