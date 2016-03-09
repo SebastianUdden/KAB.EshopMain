2b2590c469ed7fed9c7ad8db4a50f98ae5203f39
SebastianUdden <sebastian.udden@gmail.com>
2016-03-09 15:39:46 +0100

    Viewmodel etc

--- EshopMVC/app.js
+++ EshopMVC/app.js
@@ -1,26 +1,36 @@
-﻿var app = angular.module('minMax', [
-    'jcs-autoValidate',
-]);
-
-app.run(function (defaultErrorMessageResolver) {
-    defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
-        errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
-        errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
-        errorMessages['badUsername'] = 'Username can only contain numbers, letters and _';
+﻿$(document).ready(function () {
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+    }).on('hidden.bs.collapse', function () {
+        $(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
     });
 });
 
-app.controller('MinMaxCtrl', function ($scope, $http) {
-    $scope.formModel = {};
-    $scope.onSubmit = function () {
-        console.log("Hey I'm submitted!");
-        console.log($scope.formModel);
+//var app = angular.module('minMax', [
+//    'jcs-autoValidate',
+//]);
 
-        $http.post('', $scope.formModel).
-        success(function (data) {
-            console.log(":)")
-        }).error(function (data) {
-            console.log(":(")
-        });
-    };
-});
+//app.run(function (defaultErrorMessageResolver) {
+//    defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+//        errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+//        errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
+//        errorMessages['badUsername'] = 'Username can only contain numbers, letters and _';
+//    });
+//});
+
+//app.controller('MinMaxCtrl', function ($scope, $http) {
+//    $scope.formModel = {};
+//    $scope.onSubmit = function () {
+//        console.log("Hey I'm submitted!");
+//        console.log($scope.formModel);
+
+//        $http.post('', $scope.formModel).
+//        success(function (data) {
+//            console.log(":)")
+//        }).error(function (data) {
+//            console.log(":(")
+//        });
+//    };
+//});
+
+
